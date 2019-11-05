01082dc27ccb6821a0dcc043b0524acbe0e84564
Robert Rajakone <robert.rajakone@fhnw.ch>
2019-11-05 15:43:52 +0100

    buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,26 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'SensorReportViewer';
+  data = null
+  disabledOptions = {
+    'sensors': {},
+    'metrics': {},
+    'partitioners': {}
+  } 
+
   constructor(http: HttpClient){
-    http.get('/assets/export_data.json').subscribe(console.log)
+    http.get('/assets/export_data.json').subscribe(obj => {
+      this.data = obj
+      console.log(obj)
+    })
+  }
+
+  toggleButton(config: string, name: string, newState: boolean){
+    console.log(config, name, newState)
+    if (this.disabledOptions[config] === undefined){
+      this.disabledOptions[config] = {}
+    }
+
+    this.disabledOptions[config][name] = newState
   }
 }
