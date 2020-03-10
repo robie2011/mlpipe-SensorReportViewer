d3a927ffa6840be2f4bfd3737bb3878a8c27c722
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-03-10 10:58:11 +0100

    cleaning

--- src/app/reporting/reporting.component.ts
+++ src/app/reporting/reporting.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalyticsDataFacadeService } from '../analytics-data-facade.service';
 import { combineLatest } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
+import { tap, map, skip } from 'rxjs/operators';
 
 const VIEW_UPDATE_DELAY = 500
 
@@ -26,10 +26,11 @@ export class ReportingComponent implements OnInit {
     this.analyticsDataFacade.dynamicFilterOptions$
   )
 
+
+
   constructor(
     public analyticsDataFacade: AnalyticsDataFacadeService) { }
     private name: string
-    needReload: boolean = false
 
   getViewValues(arr){
     if (arr instanceof(Array)) return arr
@@ -38,18 +39,11 @@ export class ReportingComponent implements OnInit {
 
   ngOnInit() {
     this.groupsAndSettings
-    this.reload()
-  }
-
-  reload = () => {
     this.analyticsDataFacade.downloadAndSetup(this.name)
-    this.needReload = false
   }
 
   updateFilter(filterId: number, selectionIds: number[]){
     this.analyticsDataFacade.filterIdAndSelectionsUpdater.next(
       [filterId, selectionIds])
   }
-
-  
 }
