426698cda1ec2e6ba171f7a6adb6def3ac28af8d
Robert Rajakone <robert.rajakone@fhnw.ch>
2020-03-11 00:41:14 +0100

    sensor-data service: refactoring observers

--- src/app/sensor-data.service.ts
+++ src/app/sensor-data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject, combineLatest, ReplaySubject, BehaviorSubject, of } from 'rxjs';
-import { map, filter, distinctUntilChanged } from 'rxjs/operators';
+import { Subject, combineLatest, ReplaySubject, BehaviorSubject, of, Observable } from 'rxjs';
+import { map, filter, distinctUntilChanged, shareReplay } from 'rxjs/operators';
 import { SensorViewData } from './data-processor';
 import { clone } from './utils';
 import { CachedData, PartitionerInfo, createCachedData } from './data-preprocessing';
@@ -11,68 +11,20 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class SensorDataService {
-  private cachedData = new ReplaySubject<CachedData>()
-  public viewData = new ReplaySubject<SensorViewData>()
+  private cachedData: Observable<CachedData>
+  public viewData: Observable<SensorViewData>
+
   public settings = new BehaviorSubject<Settings>(null)
   public filterSettings = new BehaviorSubject<FilterSelection[]>([])
 
   constructor() {
-    this.downloadAndSetup()
+    this.cachedData = this.downloadData()
+    this.setupCachedDataReceivedHandler();
+    this.createViewDataObs();
+    this.setupViewDataMainGroupChangedHandler();
   }
 
-
-  downloadAndSetup = () => {
-    // setup initial settings
-    this.cachedData.subscribe(cache => {
-      let findMaxPartitions = [... cache.partitioners]
-      const largestPartitionerId = findMaxPartitions.sort((a,b) => a.partitionIds.length - b.partitionIds.length).reverse()[0].id
-
-      console.log("sending initial settings")
-      this.settings.next({
-        sensorNames: cache.sensors,
-        sensorSelected: [0],
-        metricNames: cache.metrics,
-        metricsSelected: cache.metrics.map((_, ix) => ix),
-        groupNames: cache.groupers,
-        groupSelected: [largestPartitionerId]
-      })
-    })
-
-    // setup generating view data
-    combineLatest(this.cachedData, this.settings, this.filterSettings)
-      .pipe(
-        // on first execution we have null value for setting
-        filter(([, settings]) => settings != null),
-        map(
-          ([cache, settings, filters]) => {
-
-
-            let viewData = cache.getViewData(
-              settings.groupSelected[0],
-              settings.sensorSelected[0],
-              filters || []) // partitionerToPartitionIx
-            return viewData
-
-          }
-        )
-      ).subscribe(v => this.viewData.next(v))
-
-      // setup: create new filtersettings if main group has changed
-      this.viewData.pipe(distinctUntilChanged( (a, b) => a.mainGroupId == b.mainGroupId)).subscribe(v => {
-        let filters = v.filters.filter(f => f.id !== v.mainGroupId)
-        .map(f => new FilterSelection(f.id, f.name, f.partitionNames, f.partitionIds))
-
-        this.filterSettings.next(filters)
-
-      })
-
-    this.downloadData().subscribe(data => {
-      console.log("loading data")
-      this.cachedData.next(data);
-    });
-  }
-
-  updateSelection = (name: string, value: number[]) => {
+  updateSettings = (name: string, value: number[]) => {
     let v = this.settings.getValue()
     if (!v) {
       console.warn("looks like we are trying to update settings without having any settings")
@@ -103,7 +55,42 @@ export class SensorDataService {
     this.settings.next(v)
   }
 
-  private downloadData(){
+  private setupViewDataMainGroupChangedHandler() {
+    this.viewData.pipe(distinctUntilChanged((a, b) => a.mainGroupId == b.mainGroupId)).subscribe(v => {
+      let filters = v.filters.filter(f => f.id !== v.mainGroupId)
+        .map(f => new FilterSelection(f.id, f.name, f.partitionNames, f.partitionIds));
+      this.filterSettings.next(filters);
+    });
+  }
+
+  private setupCachedDataReceivedHandler() {
+    this.cachedData.subscribe(cache => {
+      let findMaxPartitions = [...cache.partitioners];
+      const largestPartitionerId = findMaxPartitions.sort((a, b) => a.partitionIds.length - b.partitionIds.length).reverse()[0].id;
+      console.log("sending initial settings");
+      this.settings.next({
+        sensorNames: cache.sensors,
+        sensorSelected: [0],
+        metricNames: cache.metrics,
+        metricsSelected: cache.metrics.map((_, ix) => ix),
+        groupNames: cache.groupers,
+        groupSelected: [largestPartitionerId]
+      });
+    });
+  }
+
+  private createViewDataObs() {
+    this.viewData = combineLatest(this.cachedData, this.settings, this.filterSettings)
+      .pipe(
+        // on first execution we have null value for setting
+        filter(([cache, settings, filters]) => settings != null), map(([cache, settings, filters]) => {
+          let viewData = cache.getViewData(settings.groupSelected[0], settings.sensorSelected[0], filters || []); // partitionerToPartitionIx
+          return viewData;
+        }),
+        shareReplay(1))
+  }
+
+  private downloadData() {
     let rawData = environment.production ? window['sensor_data'] : JSON.parse(window.localStorage.getItem("data"))
     let data = createCachedData(rawData)
     return of(data)
